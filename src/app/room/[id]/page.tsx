5cd3c5a6e153a67b7bd3140c119bce923a6cd448
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2023-03-12 15:40:56 +0300

    game info now uses socket communication

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { realtime } from '@/libs/altogic';
 import type { EventData } from 'altogic/src/types';
 
 import Board from '@/components/Board';
+import { copyToClipboard, getDataFromSessionStorage, getMembers } from '@/helpers';
+import { GameType } from '@/app/page';
 
 export enum Color {
   White = 'white',
@@ -14,76 +16,96 @@ export enum Color {
 
 const COPY_TEXT_DEFAULT = 'Click to copy link';
 
-const RoomPage = ({ params: { id } }: { params: { id: string } }) => {
-  const { get } = useSearchParams();
-  const myColor = get('color') as Color;
-  const rivalColor = myColor === Color.White ? Color.Black : Color.White;
+type RoomPageProps = {
+  params: {
+    id: string;
+  };
+};
 
+const RoomPage = ({ params: { id } }: RoomPageProps) => {
+  const [pageReady, setPageReady] = useState(false);
   const [connected, setConnected] = useState(false);
   const [copyText, setCopyText] = useState(COPY_TEXT_DEFAULT);
-  const [url, setUrl] = useState(new URL(location.href));
-  const [gameStarted, setGameStarterd] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
 
-  const socketId = useRef<undefined | string>(realtime.getSocketId());
+  const gameData = getDataFromSessionStorage<{ color: Color; type: GameType }>('gameData');
+  const [myColor, setMyColor] = useState(gameData?.color);
 
   const router = useRouter();
 
   useEffect(() => {
-    realtime.join(id);
-    realtime.onConnect(onConnect);
+    console.log({ myColor });
+  }, [myColor]);
+
+  useEffect(() => {
     realtime.onJoin(onJoin);
+    realtime.onLeave(onLeave);
     realtime.onDisconnect(onDisconnect);
-    const newURL = new URL(location.href);
-    newURL.searchParams.set('color', rivalColor);
-    setUrl(newURL);
+    realtime.on('color', onColor);
+
+    getMembers(id).then(({ roomAvailable }) => {
+      if (roomAvailable) {
+        realtime.join(id);
+        setPageReady(true);
+      } else {
+        // TODO: handle room is not available
+      }
+    });
+
     return () => {
       realtime.leave(id);
-      realtime.offAny();
+      realtime.off('channel:join', onJoin);
+      realtime.off('color', onColor);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
+  }, []);
 
-  const isMe = (id: string) => id === socketId.current;
+  const isMe = (id: string) => id === realtime.getSocketId();
 
-  const copyURL = () => {
-    if (!navigator.clipboard) {
-      const el = document.createElement('textarea');
-      el.value = url.toString();
-      document.body.appendChild(el);
-      el.select();
-      document.execCommand('copy');
-      document.body.removeChild(el);
-    } else {
-      navigator.clipboard.writeText(url.toString());
-    }
+  const copyURL = async (url: string) => {
+    await copyToClipboard(url);
     setCopyText('Copied!');
-    setTimeout(() => {
-      setCopyText(COPY_TEXT_DEFAULT);
-    }, 2000);
-  };
-
-  const onConnect = () => {
-    setConnected(true);
-    socketId.current = realtime.getSocketId();
+    setTimeout(() => setCopyText(COPY_TEXT_DEFAULT), 2000);
   };
 
   const onDisconnect = () => {
-    setConnected(false);
-    socketId.current = undefined;
-    setGameStarterd(false);
+    setConnected(realtime.isConnected());
+    setGameStarted(false);
   };
 
-  const onJoin = (payload: EventData) => {
-    setGameStarterd(true);
+  const onColor = (payload: EventData) => {
+    if (isMe(payload.message.socketId)) return;
+    setMyColor(payload.message.gameData.color === Color.White ? Color.Black : Color.White);
+  };
+
+  const onJoin = async (payload: EventData) => {
+    setConnected(realtime.isConnected());
+    const { members } = await getMembers(id);
+    setGameStarted(members.length === 2);
+
     if (isMe(payload.message.id)) return;
-    console.log('Joined room', payload);
+
+    console.log('Someone joined to the room');
+    realtime.send(id, 'color', {
+      gameData,
+      socketId: realtime.getSocketId(),
+    });
+  };
+
+  const onLeave = async (payload: EventData) => {
+    setConnected(realtime.isConnected());
+    if (!isMe(payload.message.id)) console.log('Someone left from the room');
+    const { members } = await getMembers(id);
+    setGameStarted(members.length === 2);
   };
 
   const handleLeaveRoom = () => {
-    realtime.leave(id);
     router.push('/');
+    realtime.leave(id);
   };
 
+  // TODO handle loading or error
+  if (!pageReady) return null;
   return (
     <section className="py-4 flex flex-col items-center justify-evenly h-full">
       <div className="fixed right-2 top-2 text-xs text-neutral-300 flex items-center">
@@ -92,13 +114,13 @@ const RoomPage = ({ params: { id } }: { params: { id: string } }) => {
       </div>
 
       {!gameStarted && (
-        <div onClick={copyURL} className="flex flex-col items-center gap-3 w-full px-4">
+        <div onClick={() => copyURL(location.href)} className="flex flex-col items-center gap-3 w-full px-4">
           <div className="copy-text">{copyText}</div>
-          <div className="link overflow-hidden">{url.toString()}</div>
+          <div className="link overflow-hidden">{location.href}</div>
           <p className="text-center text-xs text-white sm:text-xl">Send this link to your rival to connect.</p>
         </div>
       )}
-      <Board currentColor={myColor} socketId={socketId} isMe={isMe} id={id} realtime={realtime} />
+      <Board currentColor={myColor} isMe={isMe} id={id} realtime={realtime} />
       <div className="flex gap-1">
         <button
           onClick={handleLeaveRoom}
