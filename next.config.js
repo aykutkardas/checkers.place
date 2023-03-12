640ed91cf7a829ec808f05793fc37d74f72f737a
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2023-03-12 15:38:48 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
