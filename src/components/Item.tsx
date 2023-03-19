467d14d7c86ecf3820ea5466916135b69bcffa62
Aykut Kardaş <aykutkrds@gmail.com>
2023-03-19 22:23:41 +0300

    feat: improve king item view

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -16,17 +16,45 @@ interface ItemProps {
 
 const Item = ({ color, myColor, position, selected, king, onSelect, onPointerEnter, onPointerOut }: ItemProps) => (
   <Select enabled={selected}>
-    <mesh
-      castShadow
-      receiveShadow
-      position={[position[0], 0.3, position[1]]}
-      onClick={onSelect}
-      onPointerEnter={myColor === color ? onPointerEnter : undefined}
-      onPointerOut={myColor === color ? onPointerOut : undefined}
-    >
-      <cylinderGeometry args={[0.3, 0.3, king ? 0.2 : 0.1]} />
-      <meshStandardMaterial color={color} />
-    </mesh>
+    <group>
+      <mesh
+        castShadow
+        receiveShadow
+        position={[position[0], 0.3, position[1]]}
+        onClick={onSelect}
+        onPointerEnter={myColor === color ? onPointerEnter : undefined}
+        onPointerOut={myColor === color ? onPointerOut : undefined}
+      >
+        <cylinderGeometry args={[0.3, 0.3, 0.1]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+      {king && (
+        <>
+          <mesh
+            castShadow
+            receiveShadow
+            position={[position[0], 0.37, position[1]]}
+            onClick={onSelect}
+            onPointerEnter={myColor === color ? onPointerEnter : undefined}
+            onPointerOut={myColor === color ? onPointerOut : undefined}
+          >
+            <cylinderGeometry args={[0.3, 0.3, 0.02]} />
+            <meshStandardMaterial color={'yellow'} />
+          </mesh>
+          <mesh
+            castShadow
+            receiveShadow
+            position={[position[0], 0.43, position[1]]}
+            onClick={onSelect}
+            onPointerEnter={myColor === color ? onPointerEnter : undefined}
+            onPointerOut={myColor === color ? onPointerOut : undefined}
+          >
+            <cylinderGeometry args={[0.3, 0.3, 0.1]} />
+            <meshStandardMaterial color={color} />
+          </mesh>
+        </>
+      )}
+    </group>
   </Select>
 );
 
